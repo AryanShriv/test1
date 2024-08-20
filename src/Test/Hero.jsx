186bab420c6a61f8801f31d58e0541b284b45c93
AryanShriv <aryanshrivastava020010@gmail.com>
2024-08-20 19:17:31 +0530

    Redone

--- src/Test/Hero.jsx
+++ src/Test/Hero.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Hero.css"; 
-import { TypeAnimation } from "react-type-animation";
-
 import BG from "../slider.mp4"
-
 const HeroSection = () => {
   return (
     <div className="hero-container">
@@ -13,16 +10,8 @@ const HeroSection = () => {
       </video>
       <div className="hero-overlay"></div>
       <div className="hero-content">
-        <h5 className="hero-title">
-          <TypeAnimation
-            sequence={["Innovate", 1000, "Create", 1000, "Inspire", 1000]}
-            wrapper="span"
-            speed={50}
-            style={{ fontSize: "2em", display: "inline-block" }}
-            repeat={Infinity}
-          />
-        </h5>
-        <p className="tagline">Creativity is contagious, pass it on.</p>
+        <h1>Your Heading</h1>
+        <p>Your subheading or other content</p>
       </div>
     </div>
   );
