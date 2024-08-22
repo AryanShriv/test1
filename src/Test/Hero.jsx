d5c272bf7eab11955488e36bbc555afd99c75f04
AryanShriv <aryanshrivastava020010@gmail.com>
2024-08-22 22:27:04 +0530

    test4

--- src/Test/Hero.jsx
+++ src/Test/Hero.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import "./Hero.css"; 
-import BG from "../slider.mp4"
-const HeroSection = () => {
+import videoBg from "../assets/videoBg.mp4";
+import './Hero.css'
+
+const Main = () => {
   return (
-    <div className="hero-container">
-      <video className="hero-video" playsInline autoPlay loop muted>
-        <source src={BG} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="hero-overlay"></div>
-      <div className="hero-content">
-        <h1>Your Heading</h1>
-        <p>Your subheading or other content</p>
+    <div className="main">
+      <div className="overlay"></div>
+      <video src={videoBg} autoPlay loop muted />
+      <div className="content">
+        <h1>Welcome</h1>
+        <p>To my site.</p>
       </div>
     </div>
   );
 };
 
-export default HeroSection;
+export default Main;
